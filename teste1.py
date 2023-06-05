380299a54b6ca9a083df003ae63f7f92d8443b24
pablosGomes <104365552+pablosGomes@users.noreply.github.com>
2023-06-05 19:26:05 -0300

    Add files via upload

---
+++ teste1.py
@@ -0,0 +1,8 @@
+a = 33
+b = "33"
+if b > a:
+    print("b é maior que a ")
+elif b == a:
+    print("a e b são iguais")
+
+#erro
---
+++ teste2.py
@@ -0,0 +1,8 @@
+def my_fuction(fname):
+    print(fname + "Refsnes")
+
+my_fuction("Emylly")
+my_fuction("tobias")
+my_fuction("lucas")
+
+#junta os nome com a palavra "refnes"
